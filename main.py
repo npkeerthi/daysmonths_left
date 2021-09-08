d36abe5c6ff185bff54267ee46c272dd07b6b4db
N Padma Keerthi <80039790+npkeerthi@users.noreply.github.com>
2021-09-08 19:44:24 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+
+age = input("What is your current age? :" )
+exp=int(input("how many years do you want to live 😬 :"))
+
+print(f"You have",((exp)*365-int(age)*365),"days,",((exp)*52-int(age)*52)," weeks, and ",((exp)*12-int(age)*12)," months left")
+
+
+
+
+
+
+
+
+
+
